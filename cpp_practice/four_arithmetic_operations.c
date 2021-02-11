14f042a257f5eccd42eb8ad72f1cf3a586424a9e
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-02-11 23:31:47 +0900

    practice four arithmetic operatioins

---
+++ cpp_practice/four_arithmetic_operations.c
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  cout << 1 + 2 << endl;
+  cout << 3 - 2 << endl;
+  cout << 4 * 2 << endl;
+  cout << 6 / 2 << endl;
+  cout << 7 % 2 << endl;
+
+  cout << 1 + 2 * 3 << endl;
+  cout << (1 + 2) * 3 << endl;
+
+  return 0;
+}
