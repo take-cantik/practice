c0f05ecec94722b91e71d460f394a49e08b9f61d
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-02-18 12:14:12 +0900

    Test scoring multiplication table

--- cpp_practice/reference.cpp
+++ cpp_practice/reference.cpp
@@ -107,6 +107,51 @@ void example_6(void) {
   } 
 }
 
+void saiten(vector<vector<int>> &A, int &correct_count, int &wrong_count) {
+  int kuku;
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 9; j++) {
+      if (A.at(i).at(j) == (kuku = (i + 1) * (j + 1))) {
+        correct_count++;
+      } else {
+        wrong_count++;
+        A.at(i).at(j) = kuku;
+      }
+    }
+  }
+}
+
+void test_1(void) {
+  cout << "test_1" << endl;
+
+  vector<vector<int>> A(9, vector<int>(9));
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 9; j++) {
+      cin >> A.at(i).at(j);
+    }
+  }
+  
+  int correct_count = 0;
+  int wrong_count = 0;
+
+  saiten(A, correct_count, wrong_count);
+
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 9; j++) {
+      cout << A.at(i).at(j);
+      if (j < 8) {
+        cout << " ";
+      } else {
+        cout << endl;
+      }
+    }
+  }
+
+  cout << correct_count << endl;
+  cout << wrong_count << endl;
+
+}
+
 int main(void) {
   example_1();
   example_2();
@@ -114,6 +159,7 @@ int main(void) {
   example_4();
   example_5();
   example_6();
+  test_1();
 
   return 0;
 }
