0b31471cdaa5f63bd33d18de10f3815e2bf65ed4
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-03-18 01:44:45 +0900

    update recursive_func exsample

--- cpp_practice/recursive_func.cpp
+++ cpp_practice/recursive_func.cpp
@@ -94,6 +94,31 @@ void example_1(void) {
   cout << endl;
 }
 
+int count_report_num(vector<vector<int>> &children, int x) {
+  
+}
+
+void test_1(void) {
+  int N;
+  cin >> N;
+
+  vector<int> p(N);
+  p.at(0) = -1;
+  for (int i = 1; i < N; i++) {
+    cin >> p.at(i);
+  }
+
+  vector<vector<int>> children(N);
+  for (int i = 1; i < N; i++) {
+    int parent = p.at(i);
+    children.at(parent).push_back(i);
+  }
+
+  for (int i = 0; i < N; i++) {
+    cout << count_report_num(children, i) << endl;
+  }
+}
+
 int main(void) {
   example_1();
 
