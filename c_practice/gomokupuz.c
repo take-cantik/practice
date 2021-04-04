5ed561e25c8425c62c13aa48211832c925aa5484
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-04-05 04:01:50 +0900

    gomoku refactoring

--- c_practice/gomokupuz.c
+++ c_practice/gomokupuz.c
@@ -1,130 +1,107 @@
 #include <stdio.h>
 
-int is_finish(int ary[9][9], int k, int l) {
-	for (k = 0; k < 9; k++)	{
-		for (l = 0; l < 5; l++)	{
-			if (ary[k][l] != 0 && ary[k][l] == ary[k][l + 1] && ary[k][l + 2] == ary[k][l + 3] 
-				&& ary[k][l + 4] == ary[k][l] && ary[k][l + 1] == ary[k][l + 2]) {
-				if (ary[k][l] == 1)	{
-          return 1;
-				} else {
-					return 2;
+int player_five_consecutive(int ary[9][9], int i, int j) {
+  int div[4][4][2] = {
+                       {{0, 1}, {0, 2}, {0, 3}, {0, 4}},
+                       {{1, 0}, {2, 0}, {3, 0}, {4, 0}},
+                       {{1, 1}, {2, 2}, {3, 3}, {4, 4}},
+                       {{1, -1}, {2, -2}, {3, -3}, {4, -4}}
+                     };
+  int k, l, di, dj;
+  int cnt = 0;
+  for (k = 0; k < 4; k++) {
+    for (l = 0; l < 4; l++) {
+      di = i + div[k][l][0];
+      dj = j + div[k][l][1];
+      if (di <= 8 && dj <= 8 && dj >= 0) {
+        if (ary[i][j] == ary[di][dj]) {
+          cnt++;
+        } else {
+          break;
         }
-			}
-		}
-	}
-
-	for (k = 0; k < 5; k++)	{
-		for (l = 0; l < 9; l++)	{
-			if (ary[k][l] != 0 && ary[k][l] == ary[k + 1][l] && ary[k + 2][l] == ary[k + 3][l]
-				&& ary[k + 4][l] == ary[k][l] && ary[k + 1][l] == ary[k + 2][l]) {
-				if (ary[k][l] == 1) {
-					return 1;
-				} else {
-					return 2;
-        }
-			}
-		} 
-	}
-
-	for (k = 0; k < 5; k++)	{
-		for (l = 0; l < 5; l++)	{
-			if (ary[k][l] != 0 && ary[k][l] == ary[k + 1][l + 1] && ary[k + 2][l + 2] == ary[k + 3][l + 3]
-				&& ary[k + 4][l + 4] == ary[k][l] && ary[k + 1][l + 1] == ary[k + 2][l + 2]) {
-				if (ary[k][l] == 1) {
-					return 1;
-				} else {
-					return 2;
-        }
-			}
-		}
-	}
-
-	for (k = 4; k < 9; k++) {
-		for (l = 0; l < 5; l++)	{
-			if (ary[k][l] != 0 && ary[k][l] == ary[k - 1][l + 1] && ary[k - 2][l + 2] == ary[k - 3][l + 3]
-					&& ary[k - 4][l + 4] == ary[k][l] && ary[k - 1][l + 1] == ary[k - 2][l + 2]) {
-				if (ary[k][l] == 1) {
-					return 1;
-				} else {
-					return 2;
-        }
-			}
-		}
-	}
-
-	return 0;
+      } else {
+        break;
+      }
+    }
+    if (cnt == 4) {
+      return ary[i][j];
+    }
+    cnt = 0;
+  }
+  return 0;
 }
 
+int winner(int ary[9][9]) {
+  int i, j, win_player;
+  for (i = 0; i < 9; i++) {
+    for (j = 0; j < 9; j++) {
+      if (ary[i][j] != 0) {
+        if ((win_player = player_five_consecutive(ary, i, j)) != 0) {
+          printf("win: %i\n", win_player);
+          return win_player;
+        }
+      }
+    }
+  }
+  return 0;
+}
 
-int main(void) {
-	int i, j, p, coo, x, y, jud1, jud2;
-	int array[9][9];
-	int turn = 1;
+void player_scan(int *x, int *y) {
+  int num;
+  scanf("%d", &num);
 
-	printf("〜五目並べ〜\n\n座標を入力するときはx座標とy座標を続けて入力してください。\n	ex)x座標：2,y座標：3 → 座標：23\n\n");
+  *x = num % 10 - 1;
+  *y = num / 10 - 1;
+}
 
+void print_board(int ary[9][9]) {
 	printf(" 0   1 2 3 4 5 6 7 8 9\n\n");
 
+  int i, j;
 	for (i = 0; i < 9; i++) {
 		printf(" %d  ", i + 1);
 
 		for (j = 0; j < 9; j++) {
-			array[i][j] = 0;
-			printf(" %d", array[i][j]);
+			printf(" %d", ary[i][j]);
 		}
 	
 		printf("\n");
 	}
 
 	printf("\n");
+}
 
-	while (is_finish(array, jud1, jud2) == 0) {
-		turn++;
+int main(void) {
+	printf("〜五目並べ〜\n\n座標を入力するときはx座標とy座標を続けて入力してください。\n	ex)x座標：2,y座標：3 → 座標：23\n\n");
 
+	int array[9][9] = {};
+  print_board(array);
+
+  int p, x, y;
+  int win = 0;
+  int turn = 0;
+  for (; win == 0; turn++) {
 		p = turn % 2 + 1;
 		printf("プレイヤー%dは座標を入力してください。\n座標：", p);
-		scanf("%d", &coo);
-			
-		x = coo / 10 - 1;
-		y = coo % 10 - 1;
+    player_scan(&x, &y);
 
-		while (coo < 11 || coo > 99) {
+		while (x < 0 || x > 8 || y < 0 || y > 8) {
 			printf("条件の範囲内で入力してください。\n座標：");
-			scanf("%d", &coo);
-
-			x = coo / 10 - 1;
-			y = coo % 10 - 1;
+      player_scan(&x, &y);
 		}
 
-		while (array[y][x] != 0) {
+		while (array[x][y] != 0) {
 			printf("そこは既に選ばれています。\n座標：");
-			scanf("%d", &coo);
-		
-			x = coo / 10 - 1;
-			y = coo % 10 - 1;
+      player_scan(&x, &y);
 		}
 
-		printf(" 0   1 2 3 4 5 6 7 8 9\n\n");
+    array[x][y] = p;
+    print_board(array);
+    win = winner(array);
+  }
 
-		for (i = 0; i < 9; i++)	{
-			printf(" %d  ", i + 1);
-
-			for (j = 0; j < 9; j++)	{
-				if (x == j && y == i) {
-					array[i][j] = p;
-        }
-
-				printf(" %d", array[i][j]);	
-			}
-
-			printf("\n");
-		}
-
-		printf("\n");
-	}
-
-	printf("終了。プレイヤー%dの勝ちです。\n", is_finish(array, jud1, jud2));
+	printf("終了。プレイヤー%dの勝ちです。\n", win);
 
 	return 0;
 }
+
