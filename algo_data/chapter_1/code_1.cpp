2071aed80ee84717e16c6d549e27cf3343823b0a
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-07-05 23:36:02 +0900

    Solve 1_1

--- algo_data/chapter_1/code_1.cpp
+++ algo_data/chapter_1/code_1.cpp
@@ -3,9 +3,40 @@ using namespace std;
 
 int main(void) {
   int A;
+  int from, to, half;
+  int ans;
 
-  cout << "input 20 <= A < 36" << endl;
-  cin >> A;
+  for (;;) {
+    cout << "input 20 <= A < 36" << endl;
+    cin >> A;
+
+    if (A >= 20 && A < 36) {
+      break;
+    }
+  }
+
+  from = 20;
+  to = 35;
+  half = (from + to) / 2;
+
+  for (;;) {
+    if (A <= half) {
+      to = half;
+    } else {
+      from = half + 1;
+    }
+    ans = half;
+    half = (from + to) / 2;
+    
+    // cout << "from: " << from << ", to: " << to << ", half: " << half << ", ans: " << ans << endl;
+
+    if (to - from == 0) {
+      ans = half;
+      break;
+    }
+  }
+
+  cout << ans << endl;
 
   return 0;
 }
