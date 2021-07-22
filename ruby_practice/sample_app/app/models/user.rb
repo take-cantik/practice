ba24319910f1cfc670c8eb09aea62cf1e08d196e
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-07-22 22:09:26 +0900

    Add case check

--- ruby_practice/sample_app/app/models/user.rb
+++ ruby_practice/sample_app/app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
 	validates :email, presence: true,
 										length: {maximum: 255},
 										format: {with: VALID_EMAIL_REGEX},
-										uniqueness: true
+										uniqueness: {case_sensitive: false}
 end
