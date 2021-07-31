692f3c3dce8d3d2f1a69b0fab40cca7880838774
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-07-31 21:11:02 +0900

    Test login

--- ruby_practice/sample_app/test/integration/users_login_test.rb
+++ ruby_practice/sample_app/test/integration/users_login_test.rb
@@ -9,7 +9,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get login_path
     assert_template 'sessions/new'
 
-    post login_path, params: {session: {
+    post login_path, params: { session: {
       email: "",
       password: ""
     }}
@@ -18,5 +18,19 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
     get root_path
     assert flash.empty?
+  end
+
+  test "login with valid information" do
+    get login_path
+    post login_path, params: { session: {
+      email: @user.email,
+      password: 'password'
+      }}
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
