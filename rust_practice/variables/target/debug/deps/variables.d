36c8d3eda9b7d1e93218a448c1093272b06d7722
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-08-19 23:47:18 +0900

    Use variable

---
+++ rust_practice/variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+/Users/mikitakeru/works/practice/rust_practice/variables/target/debug/deps/variables: src/main.rs
+
+/Users/mikitakeru/works/practice/rust_practice/variables/target/debug/deps/variables.d: src/main.rs
+
+src/main.rs:
---
+++ rust_practice/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/mikitakeru/works/practice/rust_practice/variables/target/debug/variables: /Users/mikitakeru/works/practice/rust_practice/variables/src/main.rs
