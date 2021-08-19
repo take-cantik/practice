14b5665e9332a32a4f3577857b829827cf2c8417
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-08-19 22:51:12 +0900

    Shadow guess

--- rust_practice/guessing_game/src/main.rs
+++ rust_practice/guessing_game/src/main.rs
@@ -16,6 +16,9 @@ fn main() {
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
+
     println!("You guessed: {}", guess);
 
     match guess.cmp(&secret_number) {
