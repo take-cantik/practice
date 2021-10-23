5362ef5afe852f47dc0acec9d8f0a55256117881
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-10-23 13:34:26 +0900

    Update: hello world slack and add .env

--- go_practice/docker_sample/main.go
+++ go_practice/docker_sample/main.go
@@ -1,8 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+  "os"
+	"strings"
+	"github.com/slack-go/slack"
+)
+
+//EV put new slack events
+var EV *slack.MessageEvent
+
+//RTM use for sending events to slack
+var RTM *slack.RTM
+
+//BotToken Put your slackbot token here
+const BotToken string := os.Getenv("BOT_TOKEN")
+
+//DefaultChannel Put your default channel
+const DefaultChannel string = "#takecantik-go-practice"
 
 func main() {
-    fmt.Println("Hello, World!")
+	var api *slack.Client = slack.New(BotToken)
+
+	RTM = api.NewRTM()
+
+	go RTM.ManageConnection()
+
+	for msg := range RTM.IncomingEvents {
+		switch ev := msg.Data.(type) {
+		case *slack.ConnectedEvent:
+			fmt.Printf("Start connection with Slack\n")
+		case *slack.MessageEvent:
+			EV = ev
+			ListenTo()
+		}
+	}
+}
+
+//ListenTo excute functions under suitable conditions
+func ListenTo() {
+	switch {
+	case strings.Contains("こんにちは", EV.Text):
+		RTM.SendMessage(RTM.NewOutgoingMessage("こんにちは。", EV.Channel))
+		return
+	}
 }
 
