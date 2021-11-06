df45b00ee41eec01dc52f67998038ab75d922a87
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-11-06 16:21:08 +0900

    Get slash command

--- go_practice/slash/main.go
+++ go_practice/slash/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-  "encoding/json"
-  "io/ioutil"
+  // "encoding/json"
+  // "io/ioutil"
   "log"
   "net/http"
   "os"
   "fmt"
-  "strings"
+  // "strings"
 
   "github.com/joho/godotenv"
   "github.com/slack-go/slack"
-  "github.com/slack-go/slack/slackevents"
+  // "github.com/slack-go/slack/slackevents"
 )
 
 func main() {
@@ -21,58 +21,10 @@ func main() {
   }
   slackBotToken := os.Getenv("SLACK_BOT_TOKEN")
   api := slack.New(slackBotToken)
+  _ = api
 
-  http.HandleFunc("/slack/events", func(w http.ResponseWriter, r *http.Request) {
-    log.Println("event")
-    body, err := ioutil.ReadAll(r.Body)
-    if err != nil {
-      log.Println(err)
-      w.WriteHeader(http.StatusInternalServerError)
-      return
-    }
-
-    eventsAPIEvent, err := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionNoVerifyToken())
-    if err != nil {
-      log.Println(err)
-      w.WriteHeader(http.StatusInternalServerError)
-      return
-    }
-
-    switch eventsAPIEvent.Type {
-    case slackevents.URLVerification:
-      var res *slackevents.ChallengeResponse
-      if err := json.Unmarshal(body, &res); err != nil {
-        log.Println(err)
-        w.WriteHeader(http.StatusInternalServerError)
-        return
-      }
-      w.Header().Set("Content-Type", "text/plain")
-      if _, err := w.Write([]byte(res.Challenge)); err != nil {
-        log.Println(err)
-        w.WriteHeader(http.StatusInternalServerError)
-        return
-      }
-    case slackevents.CallbackEvent:
-      innerEvent := eventsAPIEvent.InnerEvent
-      switch event := innerEvent.Data.(type) {
-      case *slackevents.AppMentionEvent:
-        message := strings.Split(event.Text, " ")
-        if len(message) < 2 {
-          w.WriteHeader(http.StatusBadRequest)
-          return
-        }
-
-        command := message[1]
-        switch command {
-        case "ping":
-          if _, _, err := api.PostMessage(event.Channel, slack.MsgOptionText("pong", false)); err != nil {
-            log.Println(err)
-            w.WriteHeader(http.StatusInternalServerError)
-            return
-          }
-        }
-      }
-    }
+  http.HandleFunc("/tech", func(w http.ResponseWriter, r *http.Request) {
+    log.Println("ahiahi")
   })
 
   log.Println("[INFO] Server listening")
