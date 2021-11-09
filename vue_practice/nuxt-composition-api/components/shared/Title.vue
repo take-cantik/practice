142b06bfb18c062b217c4341647bf255cdd3d213
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-11-09 21:55:47 +0900

    Create nuxt project

---
+++ vue_practice/nuxt-composition-api/components/shared/Title.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1 class="title">Hello World</h1>
+</template>
+
+<style scoped>
+.title {
+  font-size: 100px;
+  color: #0a0a0a;
+}
+</style>
---
+++ vue_practice/nuxt-composition-api/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <Title />
+  </main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
