af10c273807f779c17c3caa5fff98617cbc59b79
take-cantik <miki.takeru508@mail.kyutech.jp>
2022-01-12 20:13:14 +0900

    Update: change structure of views

--- laravel_practice/docker-laravel-helloworld/backend/app/Http/Controllers/MovieController.php
+++ laravel_practice/docker-laravel-helloworld/backend/app/Http/Controllers/MovieController.php
@@ -4,14 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Movie;
+use Illuminate\View\View;
 
 class MovieController extends Controller
 {
-    public function index() {
+    public function index(): View
+    {
         $movies = Movie::all();
 
-        return view('movie', [
-            "movies" => $movies
-        ]);
+        return view('admin.movies.index')->with('movies', $movies);
     }
 }
--- laravel_practice/docker-laravel-helloworld/backend/resources/views/admin/movies/index.blade.php
+++ laravel_practice/docker-laravel-helloworld/backend/resources/views/admin/movies/index.blade.php
@@ -1,6 +1,6 @@
 <h1>Movie一覧</h1>
 
-<table>
+<table border="0.5">
   <tr>
     <th>タイトル</th>
     <th>画像</th>
