e1ab24bacde03e0f06b996b35cfa2b5fa4af0ab2
take-cantik <miki.takeru508@mail.kyutech.jp>
2022-01-12 17:50:06 +0900

    Update: use variable at view

--- laravel_practice/hello-world/app/Http/Controllers/SampleController.php
+++ laravel_practice/hello-world/app/Http/Controllers/SampleController.php
@@ -4,10 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class SampleController extends Controller
-{
-    public function index()
-    {
-        return view('sample');
+class SampleController extends Controller {
+    public function index() {
+        $fruit_list = [
+            [
+                "name" => "apple",
+                "price" => 100
+            ],
+            [
+                "name" => "banana",
+                "price" => 300
+            ],
+            [
+                "name" => "peach",
+                "price" => 500
+            ],
+        ];
+
+        $total = 0;
+        foreach($fruit_list as $fruit) {
+            $total += $fruit['price'];
+        }
+
+        return view('sample', [
+            "fruit_list" => $fruit_list,
+            "total" => $total
+        ]);
     }
 }
--- laravel_practice/hello-world/resources/views/sample.blade.php
+++ laravel_practice/hello-world/resources/views/sample.blade.php
@@ -1,1 +1,15 @@
 <h1>This is Sample page</h1>
+
+<table border="1">
+  <tr>
+    <th> フルーツ名 </th>
+  </tr>
+  @foreach($fruit_list as $fruit)
+  <tr>
+    <td> {{ $fruit["name"] }} </td>
+  </tr>
+  @endforeach
+  </tr>
+</table>
+
+<p>合計: {{ $total }}</p>
