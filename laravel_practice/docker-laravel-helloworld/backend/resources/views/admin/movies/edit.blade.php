cb9f584f8f057aab3e0e86b23db050a26f50958c
take-cantik <miki.takeru508@mail.kyutech.jp>
2022-01-12 23:57:34 +0900

    Update: edit and show view

--- laravel_practice/docker-laravel-helloworld/backend/resources/views/admin/movies/edit.blade.php
+++ laravel_practice/docker-laravel-helloworld/backend/resources/views/admin/movies/edit.blade.php
@@ -1,1 +1,9 @@
 <h1>Edit</h1>
+<form action="/admin/movies/{{ $movie->id }}" method="PUT">
+  @csrf
+  <label>タイトル</label>
+  <input type="text" name="title" id="title" value="{{ $movie->title }}">
+  <label>画像URL</label>
+  <input type="text" name="image_url" id="image_url" value="{{ $movie->image_url }}">
+  <button type="submit">編集</button>
+</form>
--- laravel_practice/docker-laravel-helloworld/backend/resources/views/admin/movies/show.blade.php
+++ laravel_practice/docker-laravel-helloworld/backend/resources/views/admin/movies/show.blade.php
@@ -1,1 +1,2 @@
-<h1>Show</h1>
+<h1>{{ $movie->title }}</h1>
+<p>{{ $movie->image_url }}</p>
