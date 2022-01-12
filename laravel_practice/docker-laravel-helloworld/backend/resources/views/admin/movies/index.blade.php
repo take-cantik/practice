c1a495ce1b93794b88c08ac6f6db4989a7f57c3c
take-cantik <miki.takeru508@mail.kyutech.jp>
2022-01-13 00:09:50 +0900

    Add: delete form

--- laravel_practice/docker-laravel-helloworld/backend/resources/views/admin/movies/index.blade.php
+++ laravel_practice/docker-laravel-helloworld/backend/resources/views/admin/movies/index.blade.php
@@ -12,7 +12,13 @@
     <th>{{ $movie->image_url }}</th>
     <th><a href="{{ route('admin.movies.show', [$movie->id]) }}">詳細</a></th>
     <th><a href="{{ route('admin.movies.edit', [$movie->id]) }}">編集</a></th>
-    {{-- <th><a href="{{ route('admin.movies.', [$movie->id]) }}">削除</a></th> --}}
+    <th>
+      <form action="/admin/movies/{{ $movie->id }}" method="POST">
+        @csrf
+        <input type="hidden" name="_method" value="DELETE">
+        <button type="submit">削除</button>
+      </form>
+    </th>
   </tr>
   @endforeach
 </table>
