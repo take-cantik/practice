c086454d5e6139ad6cc2e5175494c127823fb45c
take-cantik <miki.takeru508@mail.kyutech.jp>
2022-01-12 19:53:06 +0900

    Update: set get all movies page

--- laravel_practice/docker-laravel-helloworld/backend/app/Http/Controllers/MovieController.php
+++ laravel_practice/docker-laravel-helloworld/backend/app/Http/Controllers/MovieController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class MovieController extends Controller
 {
-    //
+    public function index() {
+        $movies = Movie::all();
+
+        return view('movie', [
+            "movies" => $movies
+        ]);
+    }
 }
---
+++ laravel_practice/docker-laravel-helloworld/backend/resources/views/movie.blade.php
@@ -0,0 +1,14 @@
+<h1>Movie一覧</h1>
+
+<table>
+  <tr>
+    <th>タイトル</th>
+    <th>画像</th>
+  </tr>
+  @foreach($movies as $movies)
+  <tr>
+    <th>{{ $movie["title"] }}</th>
+    <th>{{ $movie["image_url"] }}</th>
+  </tr>
+  @endforeach
+</table>
