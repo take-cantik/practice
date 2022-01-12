a05ece107869b6fca938e69d8a0521cb7b8c1d75
take-cantik <miki.takeru508@mail.kyutech.jp>
2022-01-12 21:54:27 +0900

    Update: create form and link to some page

--- laravel_practice/docker-laravel-helloworld/backend/resources/views/admin/movies/create.blade.php
+++ laravel_practice/docker-laravel-helloworld/backend/resources/views/admin/movies/create.blade.php
@@ -1,1 +1,8 @@
 <h1>Create</h1>
+<form action="/movies" method="POST">
+  <label>タイトル</label>
+  <input type="text" name="title" id="title">
+  <label>画像URL</label>
+  <input type="text" name="image_url" id="image_url">
+  <button type="submit">登録</button>
+</form>
--- laravel_practice/docker-laravel-helloworld/backend/resources/views/admin/movies/index.blade.php
+++ laravel_practice/docker-laravel-helloworld/backend/resources/views/admin/movies/index.blade.php
@@ -1,4 +1,5 @@
 <h1>Movie一覧</h1>
+<a href="{{ route('admin.movies.create') }}">作成</a>
 
 <table border="0.5">
   <tr>
@@ -9,6 +10,9 @@
   <tr>
     <th>{{ $movie->title }}</th>
     <th>{{ $movie->image_url }}</th>
+    <th><a href="{{ route('admin.movies.show', [$movie->id]) }}">詳細</a></th>
+    <th><a href="{{ route('admin.movies.edit.', [$movie->id]) }}">編集</a></th>
+    {{-- <th><a href="{{ route('admin.movies.', [$movie->id]) }}">削除</a></th> --}}
   </tr>
   @endforeach
 </table>
