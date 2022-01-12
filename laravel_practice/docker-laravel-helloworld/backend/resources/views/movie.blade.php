1a1114dcf6299faa547b68bbd9125c1752458b5d
take-cantik <miki.takeru508@mail.kyutech.jp>
2022-01-12 20:05:25 +0900

    Fix: varriable

--- laravel_practice/docker-laravel-helloworld/backend/resources/views/movie.blade.php
+++ laravel_practice/docker-laravel-helloworld/backend/resources/views/movie.blade.php
@@ -5,10 +5,10 @@
     <th>タイトル</th>
     <th>画像</th>
   </tr>
-  @foreach($movies as $movies)
+  @foreach($movies as $movie)
   <tr>
-    <th>{{ $movie["title"] }}</th>
-    <th>{{ $movie["image_url"] }}</th>
+    <th>{{ $movie->title }}</th>
+    <th>{{ $movie->image_url }}</th>
   </tr>
   @endforeach
 </table>
