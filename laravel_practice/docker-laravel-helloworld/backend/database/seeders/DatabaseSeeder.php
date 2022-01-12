a75d8aa5ef79826b93963f2f5b9e9fc244c65afd
take-cantik <miki.takeru508@mail.kyutech.jp>
2022-01-12 20:05:40 +0900

    Add: seeder

--- laravel_practice/docker-laravel-helloworld/backend/database/seeders/DatabaseSeeder.php
+++ laravel_practice/docker-laravel-helloworld/backend/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table('movies')->insert([
+            'title' => Str::random(10),
+            'image_url' => Str::random(20)
+        ]);
     }
 }
