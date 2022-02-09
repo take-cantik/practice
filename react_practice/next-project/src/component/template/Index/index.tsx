cb961a3d72f5ae3ca2ac94f7d65195d6dea6d6ed
take-cantik <take.cantik17@gmail.com>
2022-02-09 21:23:33 +0900

    Update: set index template styles

--- react_practice/next-project/src/component/template/Index/index.tsx
+++ react_practice/next-project/src/component/template/Index/index.tsx
@@ -1,6 +1,9 @@
+import { css } from "@emotion/react";
 import { Dispatch, ForwardedRef, Fragment, SetStateAction } from "react";
+import { colors } from "styles/themes";
 import { Container } from "~/component/layout/Container";
 import { FlexContainer } from "~/component/layout/FlexContainer";
+import { GridContainer } from "~/component/layout/GridContainer";
 import { Header } from "~/component/shared/Header";
 import { RecoilNumberBox } from "~/component/shared/RecoilNumberBox";
 import { UseStateNumberBox } from "~/component/shared/UseStateNumberBox";
@@ -11,16 +14,34 @@ export interface IndexProps {
   forwardRef?: ForwardedRef<HTMLDivElement>;
 }
 
+const main = css`
+  width: 100%;
+  padding: 44px 0;
+  background: ${colors.black.lighten[4]};
+`;
+
+const boxes = css`
+  width: 100%;
+  max-width: 512px;
+  align-self: center;
+`;
+
 export const IndexTemplate = ({ num1, setNum1 }: IndexProps): JSX.Element => {
   return (
     <Fragment>
       <Header />
-      <main>
+      <main css={main}>
         <Container>
-          <h1>Hello, world!</h1>
-          <FlexContainer justifyContent="center" alignItems="center" gap={32}>
-            <UseStateNumberBox number={num1} setNumber={setNum1} />
-            <RecoilNumberBox />
+          <FlexContainer flexDirection="column">
+            <h1>This is using useState and Recoil page!</h1>
+            <GridContainer
+              cols={{ sp: 1, tablet: 2, pc: 2 }}
+              space={32}
+              css={boxes}
+            >
+              <UseStateNumberBox number={num1} setNumber={setNum1} />
+              <RecoilNumberBox />
+            </GridContainer>
           </FlexContainer>
         </Container>
       </main>
