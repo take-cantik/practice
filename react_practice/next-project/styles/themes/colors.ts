024849d2c0afe0c391ad1ecbb0cb0cc7f4ad3db6
take-cantik <take.cantik17@gmail.com>
2022-02-09 19:52:05 +0900

    Add: styles themes

---
+++ react_practice/next-project/styles/themes/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  black: {
+    primary: "var(--black)",
+    lighten: {
+      1: "var(--black-lighten-1)",
+      2: "var(--black-lighten-2)",
+      3: "var(--black-lighten-3)",
+      4: "var(--black-lighten-4)"
+    }
+  },
+  white: "var(--white)"
+};
