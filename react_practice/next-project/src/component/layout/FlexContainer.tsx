df1a0ed7b425944e37ac25cafae8f6c5fb1df825
take-cantik <take.cantik17@gmail.com>
2022-02-09 20:43:42 +0900

    Add: gap to flex container

--- react_practice/next-project/src/component/layout/FlexContainer.tsx
+++ react_practice/next-project/src/component/layout/FlexContainer.tsx
@@ -1,5 +1,12 @@
 import { css } from "@emotion/react";
 import { ComponentPropsWithRef, ForwardedRef } from "react";
+import { breakpoints } from "styles/themes";
+
+type Device = {
+  sp: number;
+  tablet?: number;
+  pc?: number;
+};
 
 export interface FlexContainerProps extends ComponentPropsWithRef<"div"> {
   flexDirection?: "row" | "row-reverse" | "column" | "column-reverse";
@@ -18,6 +25,7 @@ export interface FlexContainerProps extends ComponentPropsWithRef<"div"> {
     | "flex-start"
     | "flex-end";
   flexWrap?: "nowrap" | "wrap" | "wrap-reverse";
+  gap?: number | Device;
   forwardRef?: ForwardedRef<HTMLDivElement>;
 }
 
@@ -26,16 +34,28 @@ export const FlexContainer = ({
   justifyContent = "start",
   alignItems = "normal",
   flexWrap = "nowrap",
+  gap = 0,
   forwardRef,
   children,
   ...props
 }: FlexContainerProps): JSX.Element => {
+  const isGapNumber = typeof gap === "number";
+
   const flexContainer = css`
     display: flex;
     flex-direction: ${flexDirection};
     justify-content: ${justifyContent};
     align-items: ${alignItems};
     flex-wrap: ${flexWrap};
+    gap: ${isGapNumber ? gap : gap.sp};
+
+    @media (min-width: ${breakpoints.sm}px) {
+      gap: ${isGapNumber ? gap : gap.tablet};
+    }
+
+    @media (min-width: ${breakpoints.md}px) {
+      gap: ${isGapNumber ? gap : gap.pc};
+    }
   `;
 
   return (
