3c38b03b07bd71d222c0b3a5e8fa62270757e9c9
take-cantik <take.cantik17@gmail.com>
2022-02-09 18:45:51 +0900

    Add: emotion

--- react_practice/next-project/src/component/shared/Header/index.tsx
+++ react_practice/next-project/src/component/shared/Header/index.tsx
@@ -1,6 +1,24 @@
-export const Header = (): JSX.Element => {
+import { css } from "@emotion/react";
+import { ComponentPropsWithRef, ForwardedRef } from "react";
+
+export interface HeaderProps extends ComponentPropsWithRef<"header"> {
+  forwardRef?: ForwardedRef<HTMLHeadingElement>;
+}
+
+const common = css`
+  width: 100%;
+  height: 44px;
+  background: var(--black);
+  color: var(--white);
+`;
+
+export const Header = ({ forwardRef }: HeaderProps): JSX.Element => {
+  const header = css`
+    ${common};
+  `;
+
   return (
-    <header>
+    <header css={header} ref={forwardRef}>
       <h1>This is Header</h1>
     </header>
   );
