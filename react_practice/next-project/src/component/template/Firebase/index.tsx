60d19a16fb4a6cfef9bb0f56984a25cb6058287b
take-cantik <take.cantik17@gmail.com>
2022-02-10 11:17:42 +0900

    Add: firebase page

---
+++ react_practice/next-project/src/component/template/Firebase/index.tsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+import { Header } from "~/component/shared/Header";
+
+export const FirebaseTemplate = () => {
+  return (
+    <Fragment>
+      <Header />
+      <h1>firebase</h1>
+    </Fragment>
+  );
+};
---
+++ react_practice/next-project/src/pages/firebase/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import { FirebaseTemplate } from "~/component/template/Firebase";
+
+const Firebase: NextPage = (): JSX.Element => {
+  return <FirebaseTemplate />;
+};
+
+export default Firebase;
