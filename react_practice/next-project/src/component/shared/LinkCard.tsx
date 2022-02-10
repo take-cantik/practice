9143d742362bae45de58cb9f74600f57c2a52ead
take-cantik <take.cantik17@gmail.com>
2022-02-10 12:22:35 +0900

    Update: index styles

--- react_practice/next-project/src/component/shared/LinkCard.tsx
+++ react_practice/next-project/src/component/shared/LinkCard.tsx
@@ -1,10 +1,27 @@
+import { css } from "@emotion/react";
 import Link from "next/link";
 import { ComponentPropsWithRef, ForwardedRef } from "react";
+import { colors } from "styles/themes";
 
 export interface LinkCardProps extends ComponentPropsWithRef<"a"> {
   path: string;
   forwardRef?: ForwardedRef<HTMLLinkElement>;
 }
+
+const common = css`
+  width: 100%;
+  max-width: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid ${colors.black.lighten[1]};
+  border-radius: 16px;
+  padding: 24px 20px;
+  font-size: 2.4rem;
+  font-weight: bold;
+  box-shadow: 0 3px 6px ${colors.black.lighten[3]};
+  cursor: pointer;
+`;
 
 export const LinkCard = ({
   path,
@@ -14,7 +31,7 @@ export const LinkCard = ({
 }: LinkCardProps): JSX.Element => {
   return (
     <Link href={path} ref={forwardRef} {...props}>
-      <a>{children}</a>
+      <a css={common}>{children}</a>
     </Link>
   );
 };
--- react_practice/next-project/src/component/template/Index/index.tsx
+++ react_practice/next-project/src/component/template/Index/index.tsx
@@ -1,16 +1,47 @@
+import { css } from "@emotion/react";
 import { Fragment } from "react";
+import { colors } from "styles/themes";
 import { Container } from "~/component/layout/Container";
+import { FlexContainer } from "~/component/layout/FlexContainer";
 import { GridContainer } from "~/component/layout/GridContainer";
 import { Header } from "~/component/shared/Header";
 import { LinkCard } from "~/component/shared/LinkCard";
+
+const main = css`
+  width: 100%;
+  height: calc(100vh - 44px);
+  padding: 44px 0;
+  background: ${colors.black.lighten[4]};
+`;
+
+const container = css`
+  display: flex;
+  flex-flow: column;
+  gap: 32px;
+`;
+
+const title = css`
+  font-size: 2.4rem;
+  font-weight: bold;
+`;
+
+const paragraph = css`
+  font-size: 1.6rem;
+`;
 
 export const IndexTemplate = () => {
   return (
     <Fragment>
       <Header />
-      <main>
-        <Container>
-          <GridContainer>
+      <main css={main}>
+        <Container css={container}>
+          <FlexContainer flexDirection="column" gap={12}>
+            <h1 css={title}>This is Next practice</h1>
+            <p css={paragraph}>
+              You can go to that page by pressing the card of your choice
+            </p>
+          </FlexContainer>
+          <GridContainer cols={{ sp: 1, tablet: 2, pc: 3 }} gap={24}>
             <LinkCard path="/state">state</LinkCard>
             <LinkCard path="/firebase">firebase</LinkCard>
           </GridContainer>
