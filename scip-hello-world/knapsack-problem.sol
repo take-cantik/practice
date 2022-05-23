f980f187e9fc2fe72d54e410a3246f2c7177e063
take-cantik <take.cantik17@gmail.com>
2022-05-23 17:12:47 +0900

    Add: knapsack problem

---
+++ scip-hello-world/knapsack-problem.sol
@@ -0,0 +1,6 @@
+solution status: optimal solution found
+objective value:                                   15
+d2w2                                                1 	(obj:2)
+d2w1                                                1 	(obj:2)
+d1w1                                                1 	(obj:1)
+d10w4                                               1 	(obj:10)
