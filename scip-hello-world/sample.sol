8825b10a400d1f4bc844b447292017c73018924c
take-cantik <take.cantik17@gmail.com>
2022-05-23 16:49:07 +0900

    Add: scip test

---
+++ scip-hello-world/sample.sol
@@ -0,0 +1,4 @@
+solution status: optimal solution found
+objective value:                                27000
+x1                                                 30 	(obj:400)
+x2                                                 50 	(obj:300)
