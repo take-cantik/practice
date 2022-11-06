908c5e008f2904c2f3ea352cc003cece8a513614
take-cantik <take.cantik17@gmail.com>
2022-11-06 23:50:58 +0900

    Fix: welcome page

--- laravel_practice/react-on-laravel/src/resources/js/components/Example.jsx
+++ laravel_practice/react-on-laravel/src/resources/js/components/Example.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function Example() {
+const Example = () => {
     return (
         <div className="container">
             <div className="row justify-content-center">
@@ -22,3 +21,4 @@ export default Example;
 if (document.getElementById('example')) {
     ReactDOM.render(<Example />, document.getElementById('example'));
 }
+
